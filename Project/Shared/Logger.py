c6fab69b85b4c5a2ddcb92d71b08eb92457a20c4
IT20A-HentenK30 <115459361+IT20A-HentenK30@users.noreply.github.com>
2022-11-19 14:33:27 +0100

    Logger
    Logger + Unit tests

---
+++ Project/Shared/Logger.py
@@ -0,0 +1,85 @@
+from datetime import datetime
+import platform
+from threading import Lock
+import os
+import inspect
+
+
+class SingletonMeta(type):
+    _instances = {}
+
+    _lock: Lock = Lock()
+
+    def __call__(cls, *args, **kwargs):
+        with cls._lock:
+            if cls not in cls._instances:
+                instance = super().__call__(*args, **kwargs)
+                cls._instances[cls] = instance
+        return cls._instances[cls]
+
+
+class Logger(metaclass=SingletonMeta):
+    
+    PATH_WINDOWS: str       = "C:\\ProgramData\\Personenerkennung\\Logs\\"
+    PATH_LINUX: str         = "/var/Personenerkennung/Logs/"
+    FILE_FORMAT: str        = "%d_%m_%Y"
+    TIME_FORMAT: str        = "[%H:%M:%S]"
+    FILE_TYPE: str          = ".txt"
+    LOGLEVEL_TRACE: str     = "[TRACE]"
+    LOGLEVEL_DEBUG: str     = "[DEBUG]"
+    LOGLEVEL_INFO: str      = "[INFO]"
+    LOGLEVEL_WARNING: str   = "[WARNING]"
+    LOGLEVEL_ERROR: str     = "[ERROR]"
+    LOGLEVEL_FATAL: str     = "[FATAL]"
+
+    def __init__(self) -> None:
+        os_name = platform.system()
+        if os_name == "Linux":
+            self.log_dir = self.PATH_LINUX
+        elif os_name == "Windows":
+            self.log_dir = self.PATH_WINDOWS
+        else: 
+            raise Exception(f"Platform {os_name} not supported")
+        if not os.path.exists(self.log_dir):
+            os.makedirs(self.log_dir)
+        super()
+    
+    def trace(self, message):
+        caller = inspect.getmodule(inspect.stack()[1][0]).__name__
+        self._write_log(self.LOGLEVEL_TRACE, caller, message)
+    
+    def debug(self, message):
+        caller = inspect.getmodule(inspect.stack()[1][0]).__name__
+        self._write_log(self.LOGLEVEL_DEBUG, caller, message)
+    
+    def info(self, message):
+        caller = inspect.getmodule(inspect.stack()[1][0]).__name__
+        self._write_log(self.LOGLEVEL_INFO, caller, message)
+    
+    def warn(self, message):
+        caller = inspect.getmodule(inspect.stack()[1][0]).__name__
+        self._write_log(self.LOGLEVEL_WARNING, caller, message)
+    
+    def error(self, message):
+        caller = inspect.getmodule(inspect.stack()[1][0]).__name__
+        self._write_log(self.LOGLEVEL_ERROR, caller, message)
+    
+    def fatal(self, message):
+        caller = inspect.getmodule(inspect.stack()[1][0]).__name__
+        self._write_log(self.LOGLEVEL_FATAL, caller, message)
+
+    def _write_log(self, loglevel, caller, message):
+        file = self._open_logfile()
+        time = datetime.now().strftime(self.TIME_FORMAT)
+        log_entry = time + loglevel + "[" + caller + "]" + message
+        file.writelines(log_entry + "\n")
+        file.close()
+
+    def _open_logfile(self):
+        file_path = self.get_file_path()
+        file = open(file_path, "a")
+        return file
+
+    def get_file_path(self) -> str:
+        return self.log_dir + datetime.now().strftime(self.FILE_FORMAT) + self.FILE_TYPE 
+    
---
+++ Test/Shared_Test/Logger_Test.py
@@ -0,0 +1,65 @@
+import unittest
+from Project.Shared.Logger import Logger
+from datetime import datetime
+from os.path import exists
+from threading import Thread
+
+class LoggerTestCase(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self._logger = Logger()
+        return super().setUp()
+
+    def _get_latest_logentry(self) -> str:
+        file_path = self._logger.get_file_path()
+        with open(file_path, "r") as file:
+            for line in file:
+                pass
+            last_line = line
+            return last_line
+
+    def test_can_write_trace(self):
+        self._logger.trace("test_can_write_trace")
+        last_log_entry = self._get_latest_logentry()
+        self.assertTrue(self._logger.LOGLEVEL_TRACE in last_log_entry)
+    
+    def test_can_write_debug(self):
+        self._logger.debug("test_can_write_debug")
+        last_log_entry = self._get_latest_logentry()
+        self.assertTrue(self._logger.LOGLEVEL_DEBUG in last_log_entry)
+    
+    def test_can_write_info(self):
+        self._logger.info("test_can_write_info")
+        last_log_entry = self._get_latest_logentry()
+        self.assertTrue(self._logger.LOGLEVEL_INFO in last_log_entry)
+    
+    def test_can_write_warning(self):
+        self._logger.warn("test_can_write_warning")
+        last_log_entry = self._get_latest_logentry()
+        self.assertTrue(self._logger.LOGLEVEL_WARNING in last_log_entry)
+    
+    def test_can_write_error(self):
+        self._logger.error("test_can_write_error")
+        last_log_entry = self._get_latest_logentry()
+        self.assertTrue(self._logger.LOGLEVEL_ERROR in last_log_entry)
+    
+    def test_can_write_fatal(self):
+        self._logger.fatal("test_can_write_fatal")
+        last_log_entry = self._get_latest_logentry()
+        self.assertTrue(self._logger.LOGLEVEL_FATAL in last_log_entry)
+    
+    def test_has_wright_entry_format(self):
+        self._logger.info("test_has_wright_entry_format")
+        time = datetime.now().strftime(self._logger.TIME_FORMAT)
+        last_log_entry = self._get_latest_logentry()
+        self.assertTrue(time in last_log_entry)
+    
+    def test_has_wright_filename(self):
+        self._logger.info("test_has_wright_filename")
+        file_path = self._logger.get_file_path()
+        self.assertTrue(exists(file_path))
+
+    def test_logger_is_singleton(self):
+        logger1 = Logger()
+        logger2 = Logger()
+        self.assertTrue(logger1 is logger2)
